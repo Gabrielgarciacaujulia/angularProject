8758a1f8c35e91d47f367e0bac5fe1cd56ca1a84
@gabriel.caujulia <gabriel.garciacaujulia@gmail.com>
2019-11-15 09:36:05 -0300

    task5Finalizada

--- src/app/mascotas/mascota-agregar/mascota-agregar.component.ts
+++ src/app/mascotas/mascota-agregar/mascota-agregar.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators, MinLengthValidator} from "@angular/forms";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  MinLengthValidator
+} from "@angular/forms";
 import { FormBuilder } from "@angular/forms";
 import { MascotasService } from "../mascotas.service";
-import { Mascota } from "src/app/mascotas/mascota"
-import {Router} from "@angular/router"
- 
+import { Mascota } from "src/app/mascotas/mascota";
+import { Router } from "@angular/router";
+
 @Component({
   selector: "app-mascota-agregar",
   templateUrl: "./mascota-agregar.component.html",
@@ -18,21 +23,19 @@ export class MascotaAgregarComponent implements OnInit {
     descripcion: ["", Validators.required]
   });
 
-  constructor(private fb: FormBuilder, private mascotasService: MascotasService) {}
-  
+  constructor(
+    private fb: FormBuilder,
+    private mascotasService: MascotasService,
+    private router: Router
+  ) {}
 
   onSubmit() {
-    
     console.log(this.mascotaForm);
-    this.mascotasService.addMascota(this.mascotaForm.value)
-  
+    this.mascotasService.addMascota(this.mascotaForm.value).subscribe(data => {
+      this.router.navigateByUrl("/mascotas-listar");
+    });
   }
-  ngOnInit() {
-    
-  }
-  private router:Router;
+  ngOnInit() {}
 
-  newChange(): void {
-    this.router.navigateByUrl('/mascotas-listar');
-}
+ 
 }
